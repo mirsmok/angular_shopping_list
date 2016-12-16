ea08b96b70c12f75330feeb504a89807dc4fa0c9
mirsmok <mirsmok@wp.pl>
2016-12-16 19:07:23 +0000

    mysql interface config

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php 
 $DB_HOST = 'localhost';
-$DB_USER = 'USER';
-$DB_PASS = 'PASS';
+$DB_USER = 'root';
+$DB_PASS = 'mastodont';
 $DB_NAME = 'DB';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
